e04312393e166fc9291fed40655fbe62a68c6aca
mknyambura <kariuki.mercie@gmail.com>
2023-01-16 11:45:00 +0300

    completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director) => {
+        return (
+          <div key={director.name}>
+            <h3>Name: {director.name}</h3>
+            <p>Movies: {director.movies.map((movie) => {
+              return (
+                <li key={movie}>{movie}</li>
+              )
+            })}</p>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => {
+        return (
+          <div key={movie.title}>
+            <h3>Name: {movie.title}</h3>
+            <p>Time: {movie.time}</p>
+            <p>Genres {movie.genres.map((genre) => {
+              return (
+                <li key={genre}>{genre}</li>
+              )
+            })}
+            </p>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Movies;
